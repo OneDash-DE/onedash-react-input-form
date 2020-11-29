74156fe4c109985ae1283d076a4b4269fd049fcc
Jonathan Bußhoff <jonathan@busshoff.net>
2020-11-29 18:20:46 +0100

    fix: allow simple array dotted path

--- src/Utils.ts
+++ src/Utils.ts
@@ -18,15 +18,19 @@ export const set = (path: string, obj: any, value: any) => {
 			currentObject[item] = value;
 			return;
 		}
-		const itemName = item.indexOf("[]") !== -1 ? item.slice(0, item.length - 2) : item;
+		const regex = /\[(.*?)\]/;
 
-		if (!currentObject[itemName]) {
-			if (item.indexOf("[]") === -1) {
-				currentObject[item] = {};
-			} else {
-				currentObject[itemName] = [];
-			}
+		if (regex.test(item)) {
+			const index = item.match(regex)?.[1];
+			if (String(index).length === 0) return;
+			const arrName = item.slice(0, item.length - (String(index).length + 2));
+
+			if (!currentObject[arrName]) currentObject[arrName] = [];
+			if (!currentObject[arrName][Number(index)]) currentObject[arrName][Number(index)] = {};
+			currentObject = currentObject[arrName][Number(index)];
+		} else {
+			if (!currentObject[item]) currentObject[item] = {};
+			currentObject = currentObject[item];
 		}
-		currentObject = currentObject[itemName];
 	});
 };
